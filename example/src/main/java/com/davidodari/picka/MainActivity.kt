ac4d3af84f9d06190c05ff2e702930db4a42df2a
Davidodari <davidkibzodari@gmail.com>
2019-08-15 01:29:25 +0300

    Init Commit

---
+++ example/src/main/java/com/davidodari/picka/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.davidodari.picka
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.davidodari.picka.core.MediaType
+import com.davidodari.picka.core.Picka
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        pick_image_button.setOnClickListener {
+            Picka.pickMedia(this, MediaType.VIDEO, arrayOf("image/jpeg", "image/png"))
+        }
+    }
+
+}
